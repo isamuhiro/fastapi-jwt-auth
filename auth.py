d96ad9ae441353462600bfdba670b2fb9b3ce5ca
isamu <isamuhirahata@gmail.com>
2021-04-24 00:46:05 -0300

    done

--- auth.py
+++ auth.py
@@ -5,10 +5,10 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
 
-class AuthHandler:
+class AuthHandler():
     security = HTTPBearer()
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    secret = "SECRET"
+    secret = 'SECRET'
 
     def get_password_hash(self, password):
         return self.pwd_context.hash(password)
@@ -18,21 +18,24 @@ class AuthHandler:
 
     def encode_token(self, user_id):
         payload = {
-            'exp': datetime.utcnow() + timedelta(days=0, minutes=30),
+            'exp': datetime.utcnow() + timedelta(days=0, minutes=5),
             'iat': datetime.utcnow(),
             'sub': user_id
         }
-
-        return jwt.encode(payload, self.secret, algorithm='HS256')
+        return jwt.encode(
+            payload,
+            self.secret,
+            algorithm='HS256'
+        )
 
     def decode_token(self, token):
         try:
             payload = jwt.decode(token, self.secret, algorithms=['HS256'])
-            return payload
+            return payload['sub']
         except jwt.ExpiredSignatureError:
-            raise HTTPException(status_code=401, detail='Signature expired')
-        except jwt.InvalidTokenError:
-            raise HTTPException(status_code=401, detail='Invalid Token')
+            raise HTTPException(status_code=401, detail='Signature has expired')
+        except jwt.InvalidTokenError as e:
+            raise HTTPException(status_code=401, detail='Invalid token')
 
     def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
         return self.decode_token(auth.credentials)
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from auth import AuthHandler
+from schemas import AuthRequest
 
 app = FastAPI()
+
+auth_handler = AuthHandler()
+users = []
+
+
+@app.post('/register', status_code=201)
+def register(auth_request: AuthRequest):
+    if any(user['username'] == auth_request.username for user in users):
+        raise HTTPException(status_code=400, detail='Username is taken')
+
+    hashed_password = auth_handler.get_password_hash(auth_request.password)
+    users.append({
+        'username': auth_request.username,
+        'password': hashed_password
+    })
+    return
+
+
+@app.post('/login')
+def login(auth_request: AuthRequest):
+    user = next(user for user in users if user['username'] == auth_request.username)
+
+    if (user is None) or (not auth_handler.verify_password(auth_request.password, user['password'])):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+
+    token = auth_handler.encode_token(user['username'])
+    return {'token': token}
 
 
 @app.get('/')
@@ -9,10 +38,9 @@ def index():
     return {'message': 'Unprotected Route'}
 
 
-@app.get('/secret')
-def secret():
-    return {'message': 'Protected route!'}
-
+@app.get('/protected')
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return {'username': username}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
